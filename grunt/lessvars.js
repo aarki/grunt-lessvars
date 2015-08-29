16302181f8643458e6bffbda2fb37580431bbc3a
Igor Raush <igor.rausch@gmail.com>
2015-08-28 18:16:34 -0700

    test for merging multiple input files

--- grunt/lessvars.js
+++ grunt/lessvars.js
@@ -18,19 +18,22 @@ export default {
     }),
     customFormat: caseConfig('test/case/custom-format', {
         format: vars => Object.keys(vars).map(name => `var ${ name } = ${ vars[name] };`).join('\n')
-    })
+    }),
+    multipleInputs: caseConfig('test/case/multi-input', {
+        format: 'json'
+    }, [ 'input.less', 'more.less' ])
 };
 
 function caseConfig(folder, options={}) {
     const files = readdirSync(folder);
-    const src = files.filter(path => path.startsWith('input'))[0];
+    const src = files.filter(path => path.startsWith('input'));
     const exp = files.filter(path => path.startsWith('expected'))[0];
     const ext = extname(exp);
     const act = 'actual' + ext;
 
     return {
         options: options,
-        src: join(folder, src),
+        src: src.map(fn => join(folder, fn)),
         dest: join(folder, act)
     };
 }
